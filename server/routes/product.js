d334db9eafc9d58d17474fc88746423575069857
yonghyun <dydgus2433@naver.com>
2020-12-07 23:48:45 +0900

    product router

---
+++ server/routes/product.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+const { Product } = require("../models/Product")
+
+//=================================
+//             Product
+//=================================
+
+router.post("image", (req, res) => {
+
+    //가져온 이미지를 저장을 해주면 된다.
+    
+})
+
+module.exports = router
